b41f387c74713c3f91bc035bff032d6efc258a52
Eman <emanabukhater2001@gmail.com>
2025-06-29 01:23:21 +0300

    practicing middleware

--- express-app/server.js
+++ express-app/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import router from './route.js';
-// Import the usernameController from controller.js
 
 const app = express();
-
 const PORT = 3000;
+// basic middleware 
+app.use('/about',( req, res, next) => {
+  console.log(`new request received at ${new Date().toISOString()}`);
+  next();
+});
+
+
 app.get('/', (req, res) => {
   // to send a response from the serverj
   res.send('Hello, Express!');
@@ -12,61 +17,10 @@ app.get('/', (req, res) => {
 }
 );
 
-// Define a new route for '/about'
 app.get('/about', (req, res) => {
-  // to send a response from the serverj
-  res.send('About this Express app!');
+  res.send('About Page');
+} );
 
-}
-);
-
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use('/user', router)
-
-// // Define a new route for '/user/:username'
-// app.get('/user/:username', usernameController
-// );
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword;
-  res.send(`You searched for: ${keyword}`);
-}
-);
-
-app.post('/users', express.json(), (req, res) => {
-
-  const { name, email } = req.body;
-  res.json({
-    message: `User ${name} with email ${email} created successfully!`
-  }
-  );
-
-});
-
-app.put('/users/:id', express.json(), (req, res) => {
-  const userId = req.params.id;
-  const { name, email } = req.body;
-  res.json({
-    message: `User with ID ${userId} updated successfully!`,
-    user: { name, email }
-  }
-  );
-
-}
-);
-app.delete('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  res.json({
-    message: `User with ID ${userId} deleted successfully!`
-  }
-  );
-
-}
-);
-app.get('/employee/:name/:id', (req, res) => {
-  const { name, id } = req.params;
-  res.json(`Employee Name: ${name}, ID: ${id}`);
-}
-);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
