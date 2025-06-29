f7dfcb05219d2c3f9cdc6cd305ceb23264eb0853
Eman <emanabukhater2001@gmail.com>
2025-06-29 22:16:27 +0300

    session

--- express-app/server.js
+++ express-app/server.js
@@ -1,31 +1,22 @@
 import express from 'express';
-import { connectDB } from './config/db.js'; // Adjust the path as necessary
-import Person from './models/Person.js'; // Adjust the path as necessary
-const app = express();
-const PORT = 3000;
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
-await connectDB(); // Ensure you have the connectDB function imported from your db.js file
 
-// to use static files serving from express app
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false, // to avoid resaving session if unmodified
+  saveUninitialized: false,// to avoid storing empty sessions
+
+}));
+
+
 app.get('/', (req, res) => {
-
+  res.send('Hello, World!');
 }
 );
-
-app.post('/person', express.json(), async (req, res) => {
-  console.log('Received person data:', req.body);
-  const { email, name, age } = req.body;
-  // Here you would typically save the person data to the database
-  const newPerson = new Person({
-    name,
-    age,
-    email
-  });
-  await newPerson.save()
-  console.log('Person saved:', newPerson);
-  res.send('Person data received');
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
