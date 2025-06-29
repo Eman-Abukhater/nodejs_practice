4d9e54b024c462f1e0e3d9026afb209f252c3d6a
Eman <emanabukhater2001@gmail.com>
2025-06-29 19:56:42 +0300

    mongodb

--- express-app/server.js
+++ express-app/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './route.js';
 import multer from 'multer';
+import mongoose from 'mongoose';
 const storage = multer.diskStorage({
   destination: './uploads',
   filename: (req, file, cb) => {
@@ -13,7 +14,14 @@ const storage = multer.diskStorage({
 const app = express();
 const PORT = 3000;
 const upload = multer( { storage: storage });
-
+const MONGODB_URL = 'mongodb+srv://emanabukhater2001:123456Eman@cluster0.7a96qau.mongodb.net/express';
+await mongoose.connect(MONGODB_URL).then(() => {
+  console.log('Connected to MongoDB');
+}
+).catch(err => {
+  console.error('MongoDB connection error:', err);
+}
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.single('image')); // to handle multipart/form-data
