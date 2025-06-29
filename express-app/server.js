0c1370370a720b794c32f6092f44791d3ca25699
Eman <emanabukhater2001@gmail.com>
2025-06-29 19:07:36 +0300

    form data

--- express-app/server.js
+++ express-app/server.js
@@ -1,13 +1,34 @@
 import express from 'express';
 import router from './route.js';
-
+import multer from 'multer';
+const storage = multer.diskStorage({
+  destination: './uploads',
+  filename: (req, file, cb) => {
+    // Use the original name of the file
+    cb(null, file.originalname);
+  }
+  
+  // Directory to save uploaded files
+}); // Use disk storage for file uploads
 const app = express();
 const PORT = 3000;
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
- res.send('Welcome to the Express App with EJS!');
+const upload = multer( { storage: storage });
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.single('image')); // to handle multipart/form-data
+
+// to use static files serving from express app
+app.get('/', (req, res) => {
+
+}
+); 
+app.post('/form', (req, res) => {
+  console.log('Form submitted:', req.body);
+  console.log('Uploaded file:', req.file);
+
+
+  res.send('Form submitted successfully!');
 }
 );
 
